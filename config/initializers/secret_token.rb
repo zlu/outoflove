4864d3eb7b265e58660283ed08fa7f3619869f6e
zhao lu <zlu@me.com>
2011-01-08 19:11:18 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Outoflove::Application.config.secret_token = '6f7d570c8f68b70b67defd92cd15fce9d67c10288a00e75ff6af7c2aabd26b8ca4c9b118694a357fc5be2acf0b951752c4d6eb5d2b00a76e66f5f97f23d99ade'
