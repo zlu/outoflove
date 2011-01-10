5842728acd01da638a373cb7f1fd32848c7d67b4
zhao lu <zlu@me.com>
2011-01-09 16:22:02 -0800

    migrated user and friend from whodeletedme, added koala dependency

--- Gemfile
+++ Gemfile
@@ -1,11 +1,8 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.3'
-gem 'pg', '0.10.0'
-
-# To use debugger (ruby-debug for Ruby 1.8.7+, ruby-debug19 for Ruby 1.9.2+)
-# gem 'ruby-debug'
-# gem 'ruby-debug19'
+gem 'rails',  '3.0.3'
+gem 'pg',     '0.10.0'
+gem 'koala',  '0.10.0'
 
 group :development do
   gem 'heroku'
---
+++ app/controllers/friends_controller.rb
@@ -0,0 +1,43 @@
+class FriendsController < ApplicationController
+  def index
+    remote_friends_ids = facebook_session.user.friend_ids
+    local_friends_ids = current_user.friends.map(&:fb_id)
+    populate_friends(local_friends_ids) if current_user.version == 1
+    @missing_friends_ids = local_friends_ids - remote_friends_ids
+    p "++++++++++++++++++++++missing friends+++++++++++++++++++"
+    p @missing_friends_ids
+    @missing_friends = current_user.friends.select{ |friend| friend.visible && @missing_friends_ids.include?(friend.fb_id)}
+#    @missing_friends = @missing_friends.paginate({ :page => params[:page], :per_page => 10 })
+#    @missing_friends = @missing_friends.sort{|x, y| x.created_at > y.created_at}
+    new_friends_ids = remote_friends_ids - local_friends_ids
+    unless new_friends_ids.nil?
+      p "++++++++++++++++++++new_friends_ids++++++++++++++++++"
+      p new_friends_ids
+      new_friends = facebook_session.users(new_friends_ids, [:name])
+      friend_array = []
+      new_friends.each do |friend|
+        friend_array = friend_array + [{ :fb_id => friend.uid.to_s, :name => friend.name }]
+      end
+      current_user.friends.create(friend_array)
+    end
+  end
+
+  def destroy
+    Friend.find(params[:id]).update_attribute(:visible, false)
+    redirect_to friends_path
+  end
+
+  private
+
+  # create friends with their id and name
+  # name is important because later we can always know regardless of their privacy setting
+  # because otherwise we may not be able to obtain their name after being unfriended
+  def populate_friends(local_ids)
+    friends = facebook_session.users(local_ids, [:name])
+    friends.each do |friend|
+      local_friend = Friend.find_by_fb_id(friend.uid.to_s)
+      local_friend.update_attribute(:name, friend.name)
+    end
+    current_user.update_attribute(:version, 2)
+  end
+end
